08cd0de177132f0a35ca9a914a722d095ed739d0
cad3 <cad3@hw.ac.uk>
2019-02-19 11:11:03 +0000

    Update CoffeShopInterface.java
    Uncoupling Discounts from CoffeeShopInterface

--- src/coffeShop/CoffeShopInterface.java
+++ src/coffeShop/CoffeShopInterface.java
@@ -14,6 +14,8 @@ public class CoffeShopInterface {
 	  private Map<String, Integer> currentOrder = new LinkedHashMap<String, Integer>();
 	  CustomerList customerList;
 	  AllOrders allorders;
+	  DiscountCheck discountCheck;
+	  AllDiscounts allDiscounts;
 	  private float totalBeforeDiscount = 0;
 	  private double totalAfterDiscount = 0;
 	  boolean isStudentDiscount;
@@ -26,6 +28,8 @@ public class CoffeShopInterface {
 		this.menu = menu;
 	    allorders = new AllOrders();
 	    customerList = new CustomerList();
+	    allDiscounts = new AllDiscounts();
+	    discountCheck = new DiscountCheck(allDiscounts.loadDiscounts());
 	    addPreviousOrders();
 	}
 	
@@ -37,11 +41,11 @@ public class CoffeShopInterface {
 		  TreeMap<Integer,ArrayList<String>> cust = allorders.loadOrders();
 	      Set<Integer> keys = cust.keySet();
 			for(Integer key: keys){
-				float totalBeforeDiscount = calcBillBeforeDiscount(cust.get(key));
+				float totalBeforeDiscount = discountCheck.calcBillBeforeDiscount(cust.get(key));
 				float totalAfterDiscount = totalBeforeDiscount;
-				Discount ds = getDiscount(cust.get(key));
+				Discount ds = discountCheck.getDiscount(cust.get(key));
 				if(ds != null)
-					totalAfterDiscount = (float) calcAfterDiscount(cust.get(key)); //assume for the previous cases student is false
+					totalAfterDiscount = (float) discountCheck.calcAfterDiscount(cust.get(key),isStudentDiscount); //assume for the previous cases student is false
 				try {
 					customerList.addCustomer(cust.get(key), totalBeforeDiscount, totalAfterDiscount);
 				} catch (DuplicateIDException | IllegalArgumentException e1) {
@@ -51,78 +55,7 @@ public class CoffeShopInterface {
 			  }
 			}
 	  }
-	
-	public Discount getDiscount(ArrayList<String> itemIds) {
-	    AllDiscounts ad = new AllDiscounts();
-		ArrayList<Discount> discounts = ad.loadDiscounts();
-		DiscountCheck dc = new DiscountCheck(discounts);
-		Discount custDiscount = dc.checkForDeals(itemIds);
-		return custDiscount;
-}
-
-  
-	public float calcBillBeforeDiscount(ArrayList<String> itemIds) {
-  // while loop
-	  float billBeforeDiscount = 0;
-	  for(String itemid : itemIds) {
-		  	billBeforeDiscount += menu.findItemId(itemid).getPrice();
-		  }
 		  
-
-	  return billBeforeDiscount;
-   }
-	
-	
-		   
-   
-   public double calcAfterDiscount(Map<String, Integer> currentOrder) {
-		  return calcAfterDiscount(currentOrder, new ArrayList<String>());
-	  }
-	  
-   public double calcAfterDiscount(Map<String, Integer> currentOrder, ArrayList<String> discountNames) {
-		  ArrayList<String> itemIds = toArrayList(currentOrder);
-		  return calcAfterDiscount(itemIds, discountNames);
-	  }
-	  
-   public double calcAfterDiscount(ArrayList<String> itemIds) {
-		  return calcAfterDiscount(itemIds, new ArrayList<String>());
-	  }
-	  
-   public double calcAfterDiscount(ArrayList<String> itemIds, ArrayList<String> discountNames) {
-		  double totalAfterDiscount = 0;
-		  while(true) {
-			  Discount dis = getDiscount(itemIds);
-			  if(dis != null) {
-				  totalAfterDiscount += dis.getPrice();
-				  discountNames.add(dis.getName());
-				  for(String itemId : dis.getDiscountCodes()) {
-					  itemIds.remove(itemId);
-				  }
-			  } else {
-				  double price = calcBillBeforeDiscount(itemIds);
-				   if (price < 0 || price > 100){
-						throw new IllegalArgumentException( "Bill not calculated properly, input items again");
-			        
-					}
-				  totalAfterDiscount += isStudentDiscount ? price * 0.9 : price;
-				  break;
-			  }
-		  }
-		  return totalAfterDiscount;
-	  }
-	  
-	  ArrayList<String> toArrayList(Map<String, Integer> order){
-		  ArrayList<String> itemIds = new ArrayList<String>();
-			
-			Set<String> keys = order.keySet();
-			for(String key: keys){
-				for(int i =0; i < order.get(key); i++) {
-					itemIds.add(key);
-				}
-			}
-			return itemIds;
-	  }
-	  
 	//need to add in bill after discount
 	  public String displayBill(Map<String, Integer> currentOrder, boolean isStudentDiscount) {
 		  this.isStudentDiscount = isStudentDiscount;
@@ -151,7 +84,7 @@ public class CoffeShopInterface {
 	  		}
 	  		
 	  		ArrayList<String> discountNames = new ArrayList<>();
-	  		totalAfterDiscount = calcAfterDiscount(currentOrder, discountNames);
+	  		totalAfterDiscount = discountCheck.calcAfterDiscount(currentOrder, discountNames,isStudentDiscount);
 	 	   if (totalAfterDiscount < 0 || totalAfterDiscount > 100){
 				throw new IllegalArgumentException( "Bill not calculated properly, input items again");
 	        
@@ -173,13 +106,11 @@ public class CoffeShopInterface {
 	  //need to update quanities
 	  public void placeOrder(Map<String, Integer> currentOrder) throws DuplicateIDException {
 			  updateItemQuantity(currentOrder);
-			  int custId = customerList.addCustomer(toArrayList(currentOrder),totalBeforeDiscount,(float)totalAfterDiscount);
+			  int custId = customerList.addCustomer(discountCheck.toArrayList(currentOrder),totalBeforeDiscount,(float)totalAfterDiscount);
 			  totalAllItemsBeforeDiscount += totalBeforeDiscount;
 			  totalAllItemsAfterDiscount += (float)totalAfterDiscount;
-			  ArrayList<String> ord = toArrayList(currentOrder);
-			  allorders.addOrder(custId, toArrayList(currentOrder));
-
-
+			  ArrayList<String> ord = discountCheck.toArrayList(currentOrder);
+			  allorders.addOrder(custId, discountCheck.toArrayList(currentOrder));
 	  }
 	  
 	  public void updateItemQuantity(Map<String, Integer> order) {
@@ -191,6 +122,4 @@ public class CoffeShopInterface {
 				menu.findItemId(key).setQuantity(quantity);
 			}
 	  }
-	  
-
 }
