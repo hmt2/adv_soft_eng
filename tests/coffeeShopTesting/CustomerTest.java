aa2b60161972d2a8d287815f2593e51f704d2a3c
SaraCooperAmun <33864905+SaraCooperAmun@users.noreply.github.com>
2019-02-20 17:37:49 +0000

    Add files via upload

--- tests/coffeeShopTesting/CustomerTest.java
+++ tests/coffeeShopTesting/CustomerTest.java
@@ -1,5 +1,4 @@
 package coffeeShopTesting;
-
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -7,25 +6,19 @@ import java.util.ArrayList;
 import org.junit.Before;
 import org.junit.Test;
 import coffeShop.*; 
-
 public class CustomerTest {
-
+    private ArrayList<String> itemIds;
 	@Before
-	public void setUp() throws Exception {
-	}
-
-	/**@Test
-	public void test() {
-		fail("Not yet implemented");
-	}**/
-	
-	// We are checking if the customer is created with a valid customerId which needs to be over 100
-	
-	@Test
-	public void testGetId() {
-		ArrayList<String> itemIds = new ArrayList<String>();
+	public void setUp() {
+		itemIds = new ArrayList<String>();
 		itemIds.add("HOT011");
 		itemIds.add("DES006");
+	}
+
+	// We are checking if the customer is created with a valid customerId which needs to be over 100
+
+	@Test
+	public void testGetId() {
 		coffeShop.Customer customer1 = new Customer(120, 25.6f, 22.4f,itemIds) ;
 	    int expectedId = 120;
 	    int actualId = customer1.getCustomerId();
@@ -35,9 +28,7 @@ public class CustomerTest {
 	//we are trying the illegal exception
 	@Test
 	public void test_fail_exception() {
-		ArrayList<String> itemIds = new ArrayList<String>();
-		itemIds.add("HOT011");
-		itemIds.add("DES006");
+
 		try {
 			coffeShop.Customer customer = new Customer(93, 25.6f, 22.4f,itemIds);
 		    fail("should throw exception");
