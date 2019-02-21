c836124ba83c2fd3032d11ade083619b3b98ece3
cad3 <cad3@hw.ac.uk>
2019-02-21 10:58:21 +0000

    Update CustomerListTest.java

--- tests/coffeeShopTesting/CustomerListTest.java
+++ tests/coffeeShopTesting/CustomerListTest.java
@@ -1,40 +1,130 @@
 package coffeeShopTesting;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+
 import java.util.ArrayList;
 
-import org.junit.Before;
-import org.junit.Test;
-import coffeShop.*; 
-public class CustomerTest {
-    private ArrayList<String> itemIds;
-	@Before
-	public void setUp() {
-		itemIds = new ArrayList<String>();
+
+import coffeShop.*;
+
+public class CustomerListTest {
+
+
+
+
+	//Check the double ID exception
+	@Test
+	public void checkDoubleIdExceptions() throws coffeShop.DuplicateIDException {
+
+		ArrayList<String> itemIds = new ArrayList<String>();
 		itemIds.add("HOT011");
 		itemIds.add("DES006");
-	}
 
-	// We are checking if the customer is created with a valid customerId which needs to be over 100
+		Customer customer1 = new Customer(120, 25.6f, 22.4f,itemIds) ;
+		Customer customer2 = new Customer(120, 25.6f, 22.4f,itemIds) ;
+		CustomerList customerList1 = new CustomerList() ;
+		customerList1.customers().put(120, customer1);
+		customerList1.customers().put(120, customer2);
 
+
+	    }
+
+	//check if it display Bill Before Discount
 	@Test
-	public void testGetId() {
+	public void displayBillBeforeDiscount() {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("COL012");
+		coffeShop.Customer customer1 = new Customer(120, 25.6f, 25.6f,itemIds) ;
+		CustomerList customerList = new CustomerList() ;
+		customer1.setDiscount(false);
+		customerList.displayBill(customer1);
+
+	    }
+//	test display Bill After Discount	
+	@Test
+	public void displayBillAfterDiscount() {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("DES006");
 		coffeShop.Customer customer1 = new Customer(120, 25.6f, 22.4f,itemIds) ;
-	    int expectedId = 120;
-	    int actualId = customer1.getCustomerId();
-	    String message=  "Failed for id = 120";
-	    assertEquals(message, expectedId, actualId);
-		}
-	//we are trying the illegal exception
-	@Test
-	public void test_fail_exception() {
+		CustomerList customerList = new CustomerList() ;
+		customerList.displayBill(customer1);
 
-		try {
-			coffeShop.Customer customer = new Customer(93, 25.6f, 22.4f,itemIds);
-		    fail("should throw exception");
-		  } catch(IllegalArgumentException e) {
-		    // expected
-		  }	
-	}
+	    }
+
+
+	@Test
+	public void ableToDelete() throws DuplicateIDException {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("DES006");
+
+		CustomerList customerList = new CustomerList() ;
+
+		int customerId= customerList.addCustomer(itemIds, 22.5f,23.4f);
+
+		customerList.deleteCustomer(customerId);
+
+
+	    }
+
+	@Test (expected = IllegalArgumentException.class)
+	public void throwExceptionDelete() throws DuplicateIDException {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("DES006");
+
+		CustomerList customerList = new CustomerList() ;
+		customerList.addCustomer(itemIds, 22.5f,23.4f);
+		customerList.deleteCustomer(104);
+
+
+	    }
+
+
+	@Test
+	public void findCustomerId() throws DuplicateIDException {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("DES006");
+
+		CustomerList customerList = new CustomerList() ;
+		Customer customer1 = new Customer(120, 25.6f, 22.4f,itemIds) ;
+		customerList.customers().put(120, customer1);
+		assertEquals(customer1,customerList.findCustomerId(120));
+
+
+	    }
+
+	@Test (expected = IllegalArgumentException.class)
+	public void notFindCustomereId() {
+		ArrayList<String> itemIds = new ArrayList<String>();
+		itemIds.add("HOT011");
+		itemIds.add("DES006");
+		coffeShop.Customer customer1 = new Customer(120, 25.6f, 22.4f,itemIds) ;
+		CustomerList customerList = new CustomerList() ;
+		customerList.findCustomerId(121);
+
+	    }
+
+	@Test
+	public void testListByCustomerId() throws DuplicateIDException {
+		ArrayList<String> itemIds1 = new ArrayList<String>();
+		itemIds1.add("HOT011");
+		itemIds1.add("DES007");
+
+		CustomerList customerList = new CustomerList() ;
+		customerList.addCustomer(itemIds1, 25.6f, 22.4f);
+		customerList.addCustomer(itemIds1, 25.6f, 22.4f);
+		customerList.loadCustomers();
+		assertEquals(2,customerList.size());
+	    }
+
+
+
+
 
 }
