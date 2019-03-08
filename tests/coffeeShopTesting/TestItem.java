477a9a2285e8b7ecb0fd04d5372de4cf59b8752e
Clare <cad3@hw.ac.uk>
2019-03-08 10:46:54 +0000

    Creating second sprint

--- tests/coffeeShopTesting/TestItem.java
+++ tests/coffeeShopTesting/TestItem.java
@@ -13,7 +13,7 @@ public class TestItem {
 	//Test that a new item is created and contains a valid id
 	@Test
 	public void testGetId() throws InvalidIdException { //makes sure that the new item is created 
-		Item item1 = new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate");
+		Item item1 = new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate", 0);
 	    String expectedId = "HOT012";
 	    String actualId = item1.getId();
 	    String message=  "Failed for id = HOT012";
@@ -23,9 +23,9 @@ public class TestItem {
 	//Checkes whether the two items are equals or not
 	@Test
 	public void testEquals() throws InvalidIdException {
-		 Item item1 = new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate");
-		 Item item2 =  new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate");
-		 Item item3 =  new Item("HOT008","Reg Latte",2.5f,"HotDrink","Regular hot chocolate");
+		 Item item1 = new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate", 0);
+		 Item item2 =  new Item("HOT012","White chocolate",2.5f,"HotDrink","Regular hot chocolate", 0);
+		 Item item3 =  new Item("HOT008","Reg Latte",2.5f,"HotDrink","Regular hot chocolate", 0);
 		  
 		 assertTrue(item1.equals(item2));
 		 assertFalse(item1.equals(item3));
@@ -41,7 +41,7 @@ public class TestItem {
     public void testItemId() throws InvalidIdException
     {
 		try {
-			Item item = new Item("CAT001","Candy",2.5f,"HotDrink","Regular white chocolate"); //the id does not begin with the right header
+			Item item = new Item("CAT001","Candy",2.5f,"HotDrink","Regular white chocolate", 0); //the id does not begin with the right header
 			fail("Invalid id supplied - should throw exception"); 
 
 		}
@@ -56,7 +56,7 @@ public class TestItem {
     public void testItemName() throws InvalidIdException
     {
 		try {
-			Item item = new Item("FOD001","",2.5f,"HotDrink","Regular white chocolate"); 
+			Item item = new Item("FOD001","",2.5f,"HotDrink","Regular white chocolate", 0); 
 			fail("Invalid id supplied - should throw exception");  
 		}
 
@@ -69,7 +69,7 @@ public class TestItem {
     //Test for invalid prize
 	@Test(expected = IllegalStateException.class) 
 	public  void invalidPrize() throws InvalidIdException {
-		Item item = new Item("FOD001","Tuna sandwich",-3f,"Food","Tuna and salad on white bread");
+		Item item = new Item("FOD001","Tuna sandwich",-3f,"Food","Tuna and salad on white bread", 0);
 	}
 	
 
