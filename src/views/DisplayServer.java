8ad955b513afc24b300000e11212d96ba06a23b0
Clare <cad3@hw.ac.uk>
2019-03-11 11:47:29 +0000

    Working MVC with queues

--- src/views/DisplayServer.java
+++ src/views/DisplayServer.java
@@ -14,24 +14,23 @@ import model.CurrentQueue;
 
 //using observer pattern
 public class DisplayServer extends JPanel implements Observer {
-	private CurrentQueue displaydata;
-	private JTextField serverText = new JTextField(15);
+	private CurrentQueue modeldata;
+	private JTextArea serverText = new JTextArea("",10,10);
 
 	// sets up general gui
-	public DisplayServer(CurrentQueue display) {
-		this.displaydata = display;
-		display.registerObserver(this);
+	public DisplayServer(CurrentQueue model) {
+		this.modeldata = model;
+		model.registerObserver(this);
 		this.add(serverText);
+		serverText.setAlignmentX(TOP_ALIGNMENT);
 		serverText.setEditable(false);
-		serverText.setHorizontalAlignment(JTextField.CENTER);
 		Font serverFont = new Font("SansSerif", Font.BOLD, 14);
 		serverText.setFont(serverFont);
 		update();
 	}
 
 	public void update() {
-		//String text = display.showQueue
-		String text = "Server ";
+		String text = "Server: \n\r" + modeldata.showCustomerBeingServed();
 		serverText.setText(text);
 	}
 }
